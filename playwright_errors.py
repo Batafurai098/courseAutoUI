ae6dac4635a6daabb013c1313efe69b8e71695b2
Batafurai098 <artyombelyaev098@gmail.com>
2026-02-02 17:11:14 +0500

    123
    123

--- playwright_errors.py
+++ playwright_errors.py
@@ -6,5 +6,8 @@ with sync_playwright() as playwright:
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-    unknown = page.locator("#unknown")
-    expect(unknown).to_be_visible()
+    # unknown = page.locator("#unknown")
+    # expect(unknown).to_be_visible()
+
+    login_button = page.get_by_test_id('login-page-login-button')
+    login_button.fill('unknown')
